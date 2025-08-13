a0fb0f1ab5f691f4bbf1212027f101e2ebbba1fc
faluthe <73045509+faluthe@users.noreply.github.com>
2025-08-12 22:20:28 -0400

    Reorganized into workspaces

---
+++ inject/src/main.rs
@@ -0,0 +1,55 @@
+use std::process::{Command, ExitCode};
+
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct Message {
+    reason: String,
+    filenames: Vec<String>,
+}
+
+fn main() -> ExitCode {
+    let output = Command::new("cargo")
+        .args(["build", "-p", "tf_rs", "--message-format=json"])
+        .output()
+        .expect("Failed to execute cargo build");
+
+    let lib_path = {
+        let mut lib_path = None;
+        for line in String::from_utf8_lossy(&output.stdout).lines() {
+            if let Ok(msg) = serde_json::from_str::<Message>(line) {
+                if msg.reason == "compiler-artifact" && !msg.filenames.is_empty() {
+                    lib_path = Some(msg.filenames[0].clone());
+                    break;
+                }
+            }
+        }
+        lib_path.expect("Could not find compiler-artifact filename")
+    };
+
+    let pid = String::from_utf8(
+        Command::new("pidof")
+            .arg("tf_linux64")
+            .output()
+            .expect("Failed to execute pidof")
+            .stdout,
+    )
+    .expect("pidof output not UTF-8")
+    .trim()
+    .to_string();
+
+    if pid.is_empty() {
+        eprintln!("No process found with name 'tf_linux64'");
+        return ExitCode::from(1);
+    }
+
+    let status = Command::new("sudo")
+        .args(["bash", "./inject/so_inject.sh", "inject", &pid, &lib_path])
+        .status()
+        .expect("Failed to execute so_inject.sh");
+    if !status.success() {
+        return ExitCode::from(1);
+    }
+
+    ExitCode::from(0)
+}
